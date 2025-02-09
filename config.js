1d16a94dd6e448b61a21cd79e9813b5b9bc33ceb
bnnne <yuelianle@gmail.com>
2025-02-08 22:21:01 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,28 @@
+const categoryPriority = ['purple', 'blue', 'green', 'yellow'];
+
+const categories = {
+    purple: {
+        name: "unstackable items",
+        words: ["bed", "elytra", "shulker box", "potion of fire resistance"],
+        color: "purple",
+        solved: false
+    },
+    blue: {
+        name: "piglin bartering experience",
+        words: ["piglin", "gold", "ender pearls", "obsidian"],
+        color: "blue",
+        solved: false
+    },
+    green: {
+        name: "can be fed into a beacon",
+        words: ["netherite ingot", "iron ingot", "diamond", "emerald"],
+        color: "green",
+        solved: false
+    },
+    yellow: {
+        name: "biomes/dimensions that can't rain",
+        words: ["desert", "savanna", "the nether", "the end"],
+        color: "yellow",
+        solved: false
+    }
+};
